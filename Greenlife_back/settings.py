91fcd1f1c381c288a6e4ca359a7756e788fc6fa4
ravwanovv05 <samidilloravshanov14@gmail.com>
2023-12-21 16:14:38 +0500

    change

--- Greenlife_back/settings.py
+++ Greenlife_back/settings.py
@@ -11,10 +11,10 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8_pb&y7rdry1ar#=w-ss%u6k(5js-8gg!bpc6@hawpdx%63*ta'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
@@ -99,8 +99,12 @@ SWAGGER_SETTINGS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
@@ -144,6 +148,9 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- main/urls.py
+++ main/urls.py
@@ -5,17 +5,19 @@ from .views.categories import CategoryGenericAPIView, CategoryParentGenericAPIVi
 from .views.feedbacks import FeedbackGenericAPIView
 from .views.price_list import PriceListGenericAPIView
 from .views.products import ProductWithCategoryGenericAPIView, OrderProductGenericAPIView, ProductGenericAPIView
+from .views.reviews import ReviewGenericAPIView
 
 urlpatterns = [
     path('parent-categories', CategoryParentGenericAPIView.as_view(), name='parent_categories'),
     path('categories/<int:parent_id>', CategoryGenericAPIView.as_view(), name='categories'),
     path('price-list', PriceListGenericAPIView.as_view(), name='price_list'),
-    path('product-list/<int:pk>', ProductGenericAPIView.as_view(), name='product_list'),
+    path('product-detail/<int:pk>', ProductGenericAPIView.as_view(), name='product_detail'),
     path('products/<int:category_id>', ProductWithCategoryGenericAPIView.as_view(), name='products'),
     path('order-product', OrderProductGenericAPIView.as_view(), name='order_product'),
     path('blog-list', BlogListGenericAPIView.as_view(), name='blog_list'),
     path('blog-detail<int:pk>', BlogDetailGenericAPIView.as_view(), name='blog_detail'),
     path('get-partners', GetPartner.as_view(), name='partners'),
-    path('partner/<int:pk>/product', ProductByPartner.as_view(), name='partner-products'),
+    path('partnerproduct<int:pk>', ProductByPartner.as_view(), name='partner-products'),
     path('feedback', FeedbackGenericAPIView.as_view(), name='feedback'),
+    path('review', ReviewGenericAPIView.as_view(), name='review'),
 ]
