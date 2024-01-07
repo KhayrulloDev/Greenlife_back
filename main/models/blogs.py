52df02aef626c7112eeb924af6441ec26a9b2ff9
KhayrulloDev <khayrullodev@khayrullodev.khayrullodev>
2024-01-07 13:05:32 +0500

    stop4

--- main/models/blogs.py
+++ main/models/blogs.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 class Blog(models.Model):
     title = models.CharField(max_length=255, verbose_name=_('Title'))
     description = models.TextField(verbose_name=_('Description'))
-    image = models.ImageField(upload_to='pics', verbose_name=_('Image'))
+    image = models.ImageField(upload_to='pics/', verbose_name=_('Image'))
     youtube_link = models.URLField(blank=True, null=True, verbose_name=_('You Tube link'))
 
     class Meta:
