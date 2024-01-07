98fefe03d595026893f0a369539bf01bd0e0de7e
KhayrulloDev <khayrullodev@khayrullodev.khayrullodev>
2024-01-07 14:33:29 +0500

    stop4

--- main/models/file.py
+++ main/models/file.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class File(models.Model):
-    file = models.FileField(upload_to='pics', verbose_name=_('File'))
+    file = models.FileField(upload_to='pics/', verbose_name=_('File'))
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name=_('Product ID'))
 
     class Meta:
